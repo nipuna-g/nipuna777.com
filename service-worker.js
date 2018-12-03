ebf776f204fc26fba52f5883fed9fd0ead23fa5b
Nipuna Gunathilake <nipuna777@gmail.com>
2018-12-04 04:48:54 +0530

    add service worker

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,6 @@ var cacheFiles =  [
 ]
 
 self.addEventListener('install', function(e) {
-    console.log('[SW] install')
-
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(cacheFiles);
@@ -17,13 +15,10 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-    console.log('[SW] activate')
-
     e.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(cacheNames.map(function(thisCache) {
                 if (thisCache !== cacheName) {
-                    console.log('[SW] removing cached files')
                     return caches.delete(this.cacheName);
                 }
             }))
@@ -32,14 +27,9 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-    console.log('[SW] fetch')
-
     e.respondWith(
         caches.match(e.request).then(function(response) {
-            console.log("[SW] cache match", e.request.url);
-
             if (response) {
-                console.log("[SW] found in cache", e.request.url);
                 return response;
             }
             return fetch(e.request);
